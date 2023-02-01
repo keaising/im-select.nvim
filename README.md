eb758ad0a56e0667850a53ac4c816d0745fb35d2
keaising <keaising@gmail.com>
2023-02-01 11:58:10 +0800

    support customize binary's name or full path

--- README.md
+++ README.md
@@ -50,14 +50,21 @@ If you want to change some settings:
 
 ```lua
 require('im_select').setup {
+
 	-- IM will be set to `default_im_select` in `normal` mode(`EnterVim` or `InsertLeave`)
 	-- For Windows/WSL, default: "1033", aka: English US Keyboard
 	-- For macOS, default: "com.apple.keylayout.ABC", aka: US
-	-- You can use `im-select` in cli to get the IM name of you preferred
+	-- You can use `im-select` in cli to get the IM's name you preferred
 	default_im_select  = "com.apple.keylayout.ABC",
 
 	-- Set to 1 if you don't want restore IM status when `InsertEnter`
 	disable_auto_restore = 0,
+
+	-- Can be binary's name or binary's full path,
+	-- e.g. 'im-select' or '/usr/local/bin/im-select'
+	-- For Windows/WSL, default: "im-select.exe"
+	-- For macOS, default: "im-select"
+	default_command = 'im-select.exe'
 }
 ```
 
--- lua/im_select.lua
+++ lua/im_select.lua
@@ -16,6 +16,47 @@ local function determine_os()
 	end
 end
 
+-- local config
+local C = {
+	-- im-select binary's name, or the binary's full path
+	default_command = "im-select.exe",
+	-- default input method in normal mode.
+	default_method_selected = "1033",
+	-- auto restore to method to latest used in insert mode when InsertEnter
+	auto_restore = true,
+}
+
+local function set_default_config()
+	local current_os = determine_os()
+	if current_os == "macOS" then
+		C.default_command = "im-select"
+		C.default_method_selected = "com.apple.keylayout.ABC"
+	elseif current_os == "Windows" or current_os == "WSL" then
+		-- WSL share same config with Windows
+		C.default_command = "im-select.exe"
+		C.default_method_selected = "1033"
+	end
+end
+
+local function set_opts(opts)
+	if opts == nil or type(opts) ~= "table" then
+		return
+	end
+
+	if opts.default_im_select ~= nil then
+		C.default_method_selected = opts.default_im_select
+	end
+
+	if opts.disable_auto_restore == 1 then
+		C.auto_restore = false
+	end
+
+	if opts.default_command ~= nil then
+		C.default_command = opts.default_command
+	end
+end
+
+
 M.setup = function(opts)
 	local current_os = determine_os()
 
@@ -24,40 +65,25 @@ M.setup = function(opts)
 		return
 	end
 
-	-- WSL share same config with Windows
-	local default_command = "im-select.exe"
-	local default_method_selected = "1033"
-	if current_os == "macOS" then
-		default_command = "im-select"
-		default_method_selected = "com.apple.keylayout.ABC"
-	end
+	set_default_config()
+	set_opts(opts)
 
-	if vim.fn.executable(default_command) ~= 1 then
+	if vim.fn.executable(C.default_command) ~= 1 then
 		vim.api.nvim_err_writeln(
 			[[please install `im-select` binary first, repo url: https://github.com/daipeihust/im-select]]
 		)
 		return
 	end
 
-	-- config
-	if opts ~= nil and opts.default_im_select ~= nil then
-		default_method_selected = opts.default_im_select
-	end
-
-	local auto_restore = true
-	if opts ~= nil and opts.disable_auto_restore == 1 then
-		auto_restore = false
-	end
-
 	-- set autocmd
-	if auto_restore then
+	if C.auto_restore then
 		vim.api.nvim_create_autocmd({ "InsertEnter" }, {
 			callback = function()
-				local current_select = all_trim(vim.fn.system({ default_command }))
+				local current_select = all_trim(vim.fn.system({ C.default_command }))
 				local save = vim.g["im_select_current_im_select"]
 
 				if current_select ~= save then
-					vim.fn.system({ default_command, save })
+					vim.fn.system({ C.default_command, save })
 				end
 			end,
 		})
@@ -65,11 +91,11 @@ M.setup = function(opts)
 
 	vim.api.nvim_create_autocmd({ "InsertLeave", "VimEnter" }, {
 		callback = function()
-			local current_select = all_trim(vim.fn.system({ default_command }))
+			local current_select = all_trim(vim.fn.system({ C.default_command }))
 			vim.api.nvim_set_var("im_select_current_im_select", current_select)
 
-			if current_select ~= default_method_selected then
-				vim.fn.system({ default_command, default_method_selected })
+			if current_select ~= C.default_method_selected then
+				vim.fn.system({ C.default_command, C.default_method_selected })
 			end
 		end,
 	})
