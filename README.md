22f136aef1dd2f1672f2d23713d6e8ce9dc43ab7
keaising <keaising@gmail.com>
2023-05-06 12:21:51 +0800

    chore: reorg and format

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ The old vim plugins (such as [im-select](https://github.com/daipeihust/im-select
 
 Current version works for NeoVim on:
 
-+ macOS
-+ Windows and WSL
-+ Fcitx5 on Linux
+- macOS
+- Windows and WSL
+- Fcitx5 on Linux
 
 Other frameworks on Linux's support is welcome!
 
@@ -16,7 +16,7 @@ Other frameworks on Linux's support is welcome!
 
 For Windows and macOS user, please install executable file `im-select` first
 
-Download URL:  [im-select](https://github.com/daipeihust/im-select)
+Download URL: [im-select](https://github.com/daipeihust/im-select)
 
 For fcitx5 user, you need to install fcitx5
 
@@ -43,13 +43,13 @@ Lazy
 
 Packer
 
-``` lua
+```lua
 use 'keaising/im-select.nvim'
 ```
 
 Plug
 
-``` vim
+```vim
 Plug 'keaising/im-select.nvim'
 ```
 
@@ -61,27 +61,30 @@ Setup is a must, and it works well enough with the default settings:
 require('im_select').setup()
 ```
 
-If you want to change some settings: 
+If you want to change some settings:
 
 ```lua
 require('im_select').setup {
 
-	-- IM will be set to `default_im_select` in `normal` mode
-	-- For Windows/WSL, default: "1033", aka: English US Keyboard
-	-- For macOS, default: "com.apple.keylayout.ABC", aka: US
-	-- For Linux, default: "keyboard-us"
-	-- You can use `im-select` or `fcitx5-remote -n` to get the IM's name you preferred
-	default_im_select  = "com.apple.keylayout.ABC",
+    -- IM will be set to `default_im_select` in `normal` mode
+    -- For Windows/WSL, default: "1033", aka: English US Keyboard
+    -- For macOS, default: "com.apple.keylayout.ABC", aka: US
+    -- For Linux, default: "keyboard-us"
+    -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name you preferred
+    default_im_select  = "com.apple.keylayout.ABC",
 
-	-- Set to 1 if you don't want restore IM status when `InsertEnter`
-	disable_auto_restore = 0,
+    -- Set to `true` if you don't want restore IM status when `InsertEnter`
+    disable_auto_restore = false,
 
-	-- Can be binary's name or binary's full path,
-	-- e.g. 'im-select' or '/usr/local/bin/im-select'
-	-- For Windows/WSL, default: "im-select.exe"
-	-- For macOS, default: "im-select"
-	-- For Linux, default: "fcitx5-remote"
-	default_command = 'im-select.exe'
+    -- Can be binary's name or binary's full path,
+    -- e.g. 'im-select' or '/usr/local/bin/im-select'
+    -- For Windows/WSL, default: "im-select.exe"
+    -- For macOS, default: "im-select"
+    -- For Linux, default: "fcitx5-remote"
+    default_command = 'im-select.exe'
+
+    -- If you would like set IM to `default_im_select` when back to NeoVim from other app,
+    -- set this option to `true`
+    set_default_im_on_focus_gained = false
 }
 ```
-
--- lua/im_select.lua
+++ lua/im_select.lua
@@ -1,148 +1,157 @@
 local M = {}
 
 local function all_trim(s)
-	return s:match("^%s*(.-)%s*$")
+    return s:match("^%s*(.-)%s*$")
 end
 
 local function determine_os()
-	if vim.fn.has("macunix") == 1 then
-		return "macOS"
-	elseif vim.fn.has("win32") == 1 then
-		return "Windows"
-	elseif vim.fn.has("wsl") == 1 then
-		return "WSL"
-	else
-		return "Linux"
-	end
+    if vim.fn.has("macunix") == 1 then
+        return "macOS"
+    elseif vim.fn.has("win32") == 1 then
+        return "Windows"
+    elseif vim.fn.has("wsl") == 1 then
+        return "WSL"
+    else
+        return "Linux"
+    end
 end
 
 local function is_supported()
-	local os = determine_os()
-	-- macOS, Windows, WSL
-	if os ~= "Linux" then
-		return true
-	end
+    local os = determine_os()
+    -- macOS, Windows, WSL
+    if os ~= "Linux" then
+        return true
+    end
 
-	-- only support fcitx5
-	-- other frameworks are not support yet, PR welcome
-	if vim.fn.executable("fcitx5-remote") then
-		return true
-	end
+    -- only support fcitx5
+    -- other frameworks are not support yet, PR welcome
+    if vim.fn.executable("fcitx5-remote") then
+        return true
+    end
 end
 
 -- local config
 local C = {
-	-- im-select binary's name, or the binary's full path
-	default_command = "im-select.exe",
-	-- default input method in normal mode.
-	default_method_selected = "1033",
-	-- set input method to default on FocusGained
-	set_default_im_on_focus_gained = false,
-	-- auto restore to method to latest used in insert mode when InsertEnter
-	auto_restore = true,
+    -- im-select binary's name, or the binary's full path
+    default_command = "im-select.exe",
+    -- default input method in normal mode.
+    default_method_selected = "1033",
+    -- set input method to default on FocusGained
+    set_default_im_on_focus_gained = false,
+    -- auto restore to method to latest used in insert mode when InsertEnter
+    auto_restore = true,
 }
 
 local function set_default_config()
-	local current_os = determine_os()
-	if current_os == "macOS" then
-		C.default_command = "im-select"
-		C.default_method_selected = "com.apple.keylayout.ABC"
-	elseif current_os == "Windows" or current_os == "WSL" then
-		-- WSL share same config with Windows
-		C.default_command = "im-select.exe"
-		C.default_method_selected = "1033"
-	else
-		-- fcitx5-remote -n: rime/keyboard-us
-		-- fcitx5-remote -s rime
-		-- fcitx5-remote -s keyboard-us
-		C.default_command = "fcitx5-remote"
-		C.default_method_selected = "keyboard-us"
-	end
+    local current_os = determine_os()
+    if current_os == "macOS" then
+        C.default_command = "im-select"
+        C.default_method_selected = "com.apple.keylayout.ABC"
+    elseif current_os == "Windows" or current_os == "WSL" then
+        -- WSL share same config with Windows
+        C.default_command = "im-select.exe"
+        C.default_method_selected = "1033"
+    else
+        -- fcitx5-remote -n: rime/keyboard-us
+        -- fcitx5-remote -s rime
+        -- fcitx5-remote -s keyboard-us
+        C.default_command = "fcitx5-remote"
+        C.default_method_selected = "keyboard-us"
+    end
 end
 
 local function set_opts(opts)
-	if opts == nil or type(opts) ~= "table" then
-		return
-	end
+    if opts == nil or type(opts) ~= "table" then
+        return
+    end
 
-	if opts.default_im_select ~= nil then
-		C.default_method_selected = opts.default_im_select
-	end
+    if opts.default_im_select ~= nil then
+        C.default_method_selected = opts.default_im_select
+    end
 
-	if opts.disable_auto_restore == 1 then
-		C.auto_restore = false
-	end
+    if opts.disable_auto_restore == 1 or opts.disable_auto_restore == true then
+        C.auto_restore = false
+    end
 
-	if opts.default_command ~= nil then
-		C.default_command = opts.default_command
-	end
+    if opts.default_command ~= nil then
+        C.default_command = opts.default_command
+    end
 
-	if opts.set_default_im_on_focus_gained == 1 then
-		C.set_default_im_on_focus_gained = true
-	end
+    if opts.set_default_im_on_focus_gained == true then
+        C.set_default_im_on_focus_gained = true
+    end
 end
 
 local function get_current_select(cmd)
-	-- fcitx5 has its own parameters
-	if cmd:find("fcitx5-remote", 1, true) ~= nil then
-		return all_trim(vim.fn.system({ cmd, "-n" }))
-	else
-		return all_trim(vim.fn.system({ cmd }))
-	end
+    -- fcitx5 has its own parameters
+    if cmd:find("fcitx5-remote", 1, true) ~= nil then
+        return all_trim(vim.fn.system({ cmd, "-n" }))
+    else
+        return all_trim(vim.fn.system({ cmd }))
+    end
 end
 
 local function change_im_select(cmd, method)
-	if cmd:find("fcitx5-remote", 1, true) then
-		print("change in im-select", cmd, method)
-		return vim.fn.system({ cmd, "-s", method })
-	else
-		return vim.fn.system({ cmd, method })
-	end
+    if cmd:find("fcitx5-remote", 1, true) then
+        print("change in im-select", cmd, method)
+        return vim.fn.system({ cmd, "-s", method })
+    else
+        return vim.fn.system({ cmd, method })
+    end
+end
+
+local function restore_default_im_status()
+    local current = get_current_select(C.default_command)
+    vim.api.nvim_set_var("im_select_saved_status", current)
+
+    if current ~= C.default_method_selected then
+        change_im_select(C.default_command, C.default_method_selected)
+    end
+end
+
+local function restore_previous_saved()
+    local current = get_current_select(C.default_command)
+    local saved = vim.g["im_select_saved_status"]
+
+    if current ~= saved then
+        change_im_select(C.default_command, saved)
+    end
 end
 
 M.setup = function(opts)
-	if not is_supported() then
-		return
-	end
+    if not is_supported() then
+        return
+    end
 
-	set_default_config()
-	set_opts(opts)
+    set_default_config()
+    set_opts(opts)
 
-	if vim.fn.executable(C.default_command) ~= 1 then
-		vim.api.nvim_err_writeln(
-			[[please install `im-select` binary first, repo url: https://github.com/daipeihust/im-select]]
-		)
-		return
-	end
+    if vim.fn.executable(C.default_command) ~= 1 then
+        vim.api.nvim_err_writeln(
+            [[please install `im-select` binary first, repo url: https://github.com/daipeihust/im-select]]
+        )
+        return
+    end
 
-	-- set autocmd
-	if C.auto_restore then
-		vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter" }, {
-			callback = function()
-				local current_select = get_current_select(C.default_command)
-				local save = vim.g["im_select_current_im_select"]
+    -- set autocmd
+    local group_id = vim.api.nvim_create_augroup("im-select", { clear = true })
 
-				if current_select ~= save then
-					change_im_select(C.default_command, save)
-				end
-			end,
-		})
-	end
+    if C.auto_restore then
+        vim.api.nvim_create_autocmd({ "InsertEnter", "CmdlineEnter", "FocusLost" }, {
+            callback = restore_previous_saved,
+            group = group_id,
+        })
+    end
 
-	local events = { "InsertLeave", "VimEnter", "CmdlineLeave" }
-	if C.set_default_im_on_focus_gained then
-		table.insert(events, "FocusGained")
-	end
-	vim.api.nvim_create_autocmd(events, {
-		callback = function()
-			local current_select = get_current_select(C.default_command)
-			vim.api.nvim_set_var("im_select_current_im_select", current_select)
+    local restore_default_events = { "InsertLeave", "VimEnter", "CmdlineLeave" }
+    if C.set_default_im_on_focus_gained then
+        table.insert(restore_default_events, "FocusGained")
+    end
 
-			if current_select ~= C.default_method_selected then
-				change_im_select(C.default_command, C.default_method_selected)
-			end
-		end,
-	})
+    vim.api.nvim_create_autocmd(restore_default_events, {
+        callback = restore_default_im_status,
+        group = group_id,
+    })
 end
 
 return M
